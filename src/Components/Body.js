795012943e129615e9ff4059f406792c84567698
Bhupesh <bhupesh@Bhupeshs-MacBook-Air.local>
2026-04-25 14:44:33 +0530

    Search functionality

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,6 +5,8 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [datas, setData] = useState([]);
+  const [inputVal, setInputVal] = useState("");
+  const [filterData, setFilterData] = useState([]);
 
   const CallFilterFunction = () => {
     let filterDasta = datas.filter((d) => {
@@ -15,7 +17,7 @@ const Body = () => {
 
   const fetcAPI = async () => {
     const fetchData = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&collection=83634&tags=layout_CCS_SouthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null",
+      "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&collection=83634&tags=layout_CCS_SouthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null",
     );
     const convertinJson = await fetchData.json();
     const mappedData = convertinJson.data.cards
@@ -24,6 +26,7 @@ const Body = () => {
       })
       .filter(Boolean);
     setData(mappedData);
+    setFilterData(mappedData);
   };
 
   useEffect(() => {
@@ -33,19 +36,47 @@ const Body = () => {
   if (datas.length === 0) {
     return <Shimmer />;
   }
+
+  const handleChange = (e) => {
+    setInputVal(e.target.value);
+  };
+
+  const handleClick = () => {
+    const filter = datas.filter((d) => {
+      return d.name.toLowerCase().includes(inputVal.toLowerCase());
+    });
+    setFilterData(filter);
+  };
+
   return (
     <>
-      <button
-        onClick={CallFilterFunction}
-        className="p-3.5 border-8 cursor-pointer"
-        type="button"
-      >
-        Press to filter the list
-      </button>
+      <div className="flex">
+        <div className=" flex items-center justify-center bg-amber-300 align-middle gap-3.5">
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            value={inputVal}
+            placeholder="Search item"
+            className="p-2 border rounded"
+          />
+          <button
+            onClick={handleClick}
+            className="border-2 cursor-pointer px-4 mr-3.5  rounded-b-sm"
+          >
+            Search
+          </button>
+        </div>
+        <button
+          onClick={CallFilterFunction}
+          className="p-3.5 border-8 cursor-pointer"
+          type="button"
+        >
+          Press to filter the list
+        </button>
+      </div>
       <div className="bg-amber-200 p-5">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {}
-          {datas.map((data, index) => (
+          {filterData.map((data, index) => (
             <RestourantCard item={data} key={index} />
           ))}
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,28 @@
 import { IMG_URL } from "../../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [text, setText] = useState("LOG IN");
+
+  const handleClick = () => {
+    text == "LOG IN" ? setText("LOG OUT") : setText("LOG IN");
+  };
+
   return (
     <>
       <div className="flex flex-wrap items-center justify-between bg-amber-600 p-3">
-        <img
-          className="h-16 rounded-2xl"
-          src={IMG_URL}
-        />
+        <img className="h-16 rounded-2xl" src={IMG_URL} />
 
         <ul className="flex gap-6 font-bold text-white mt-2 sm:mt-0">
           <li>Home</li>
           <li>Contact</li>
           <li>About</li>
+          <div
+            onClick={handleClick}
+            className="p-2 bg-amber-900 rounded-2xl text-zinc-50 cursor-pointer "
+          >
+            <button className="cursor-pointer">{text}</button>
+          </div>
         </ul>
       </div>
     </>
